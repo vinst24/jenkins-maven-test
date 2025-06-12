7868840c6c0c9a02273d13fc6a04deb4cfe8c5e0
Nguyễn Phúc Vinh <127533122+vinst24@users.noreply.github.com>
2025-06-12 07:38:22 +0700

    Update GradeConverter.java

--- src/main/java/GradeConverter.java
+++ src/main/java/GradeConverter.java
@@ -4,11 +4,11 @@ public class GradeConverter {
     {
         if(score <= 100 && score >= 90) {      return "A";
         } else if(score <= 89 && score >= 80) {     return "B";
-        } else if(score <= 79 && score > 70){
+        } else if(score <= 79 && score >= 70){
             return "C";
-        } else if(score <= 69 && score > 60) {
+        } else if(score <= 69 && score >= 60) {
             return "D";
-        } else if(score <= 59 && score > 0) {
+        } else if(score <= 59 && score >= 0) {
             return "E";
         } else {
             return "Invalid";
